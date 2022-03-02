6eec34a3866492b05954c8fe45aea2d11bde74a0
Grupo Colav <grupocolav@udea.edu.co>
2022-03-02 02:13:02 +0100

    working in EN_RED

--- kaypacha/models/scienti/2018/graph_fields.py
+++ kaypacha/models/scienti/2018/graph_fields.py
@@ -1,160 +1,208 @@
 graph_fields = {}
-graph_fields["EN_PRODUCTO"] = {"alias": "product"}
-graph_fields["EN_PRODUCTO"]["fields"] = {
+
+##
+# product fields
+##
+
+product_fields = {}
+product_fields["EN_PRODUCTO"] = {"alias": "product"}
+product_fields["EN_PRODUCTO"]["fields"] = {
 }
 
-graph_fields["EN_INSTITUCION"] = {"alias": "institution"}
-graph_fields["EN_INSTITUCION"]["fields"] = {
+product_fields["EN_INSTITUCION"] = {"alias": "institution"}
+product_fields["EN_INSTITUCION"]["fields"] = {
 }
 
-
-graph_fields["EN_RECURSO_HUMANO"] = {"alias": "author"}
-graph_fields["EN_RECURSO_HUMANO"]["fields"] = {
+product_fields["EN_RECURSO_HUMANO"] = {"alias": "author"}
+product_fields["EN_RECURSO_HUMANO"]["fields"] = {
 }
 
-graph_fields["EN_GRUPO_PESQUISA"] = {"alias": "group"}
-graph_fields["EN_GRUPO_PESQUISA"]["fields"] = {
+product_fields["EN_GRUPO_PESQUISA"] = {"alias": "group"}
+product_fields["EN_GRUPO_PESQUISA"]["fields"] = {
 }
 
-graph_fields["EN_AREA_CONOCIMIENTO"] = {"alias": "knowledge_area"}
-graph_fields["EN_AREA_CONOCIMIENTO"]["fields"] = {
+product_fields["EN_AREA_CONOCIMIENTO"] = {"alias": "knowledge_area"}
+product_fields["EN_AREA_CONOCIMIENTO"]["fields"] = {
 }
 
-graph_fields["EN_RECURSO_HUMANO_OTRO"] = {"alias": "author_others"}
-graph_fields["EN_RECURSO_HUMANO_OTRO"]["fields"] = {
+product_fields["EN_RECURSO_HUMANO_OTRO"] = {"alias": "author_others"}
+product_fields["EN_RECURSO_HUMANO_OTRO"]["fields"] = {
 }
 
-graph_fields["EN_MUNICIPIO"] = {"alias": "city"}
-graph_fields["EN_MUNICIPIO"]["fields"] = {
+product_fields["EN_MUNICIPIO"] = {"alias": "city"}
+product_fields["EN_MUNICIPIO"]["fields"] = {
 }
 
-graph_fields["EN_PROYECTO_GR"] = {"alias": ""}
-graph_fields["EN_PROYECTO_GR"]["fields"] = {
+product_fields["EN_PROYECTO_GR"] = {"alias": ""}
+product_fields["EN_PROYECTO_GR"]["fields"] = {
 }
 
+product_fields["EN_PRODUCTO_GR"] = {"alias": "", }
+product_fields["EN_PRODUCTO_GR"]["fields"] = {}
 
-graph_fields["EN_PRODUCTO_GR"] = {"alias": "", }
-graph_fields["EN_PRODUCTO_GR"]["fields"] = {}
-
-
-graph_fields["EN_TIPO_PRODUCTO"] = {"alias": "producto_type"}
-graph_fields["EN_TIPO_PRODUCTO"]["fields"] = {
+product_fields["EN_TIPO_PRODUCTO"] = {"alias": "producto_type"}
+product_fields["EN_TIPO_PRODUCTO"]["fields"] = {
 }
 
-graph_fields["EN_PROYECTO"] = {"alias": "project"}
-graph_fields["EN_PROYECTO"]["fields"] = {
+product_fields["EN_PROYECTO"] = {"alias": "project"}
+product_fields["EN_PROYECTO"]["fields"] = {
 }
 
-graph_fields["EN_PROD_TECNICA"] = {
+product_fields["EN_PROD_TECNICA"] = {
     "alias": "technical", "sub_section": "details"}
-graph_fields["EN_PROD_TECNICA"]["fields"] = {
+product_fields["EN_PROD_TECNICA"]["fields"] = {
 }
 
-
-graph_fields["EN_PATENTE"] = {"alias": "patent", "sub_section": "details"}
-graph_fields["EN_PATENTE"]["fields"] = {
+product_fields["EN_PATENTE"] = {"alias": "patent", "sub_section": "details"}
+product_fields["EN_PATENTE"]["fields"] = {
 }
 
-graph_fields["EN_PROD_TECNOLOGICO"] = {
+product_fields["EN_PROD_TECNOLOGICO"] = {
     "alias": "tech_product", "sub_section": "details"}
-graph_fields["EN_PROD_TECNOLOGICO"]["fields"] = {
+product_fields["EN_PROD_TECNOLOGICO"]["fields"] = {
 }
 
-graph_fields["EN_SECRETO_INDUSTRIAL"] = {
+product_fields["EN_SECRETO_INDUSTRIAL"] = {
     "alias": "industrial_secret", "sub_section": "details"}
-graph_fields["EN_SECRETO_INDUSTRIAL"]["fields"] = {
+product_fields["EN_SECRETO_INDUSTRIAL"]["fields"] = {
 }
 
-
-graph_fields["EN_PROD_SOFTWARE"] = {
+product_fields["EN_PROD_SOFTWARE"] = {
     "alias": "software", "sub_section": "details"}
-graph_fields["EN_PROD_SOFTWARE"]["fields"] = {
+product_fields["EN_PROD_SOFTWARE"]["fields"] = {
 }
 
 
-graph_fields["EN_REGISTRO"] = {"alias": "register", "sub_section": "details"}
-graph_fields["EN_REGISTRO"]["fields"] = {
+product_fields["EN_REGISTRO"] = {"alias": "register", "sub_section": "details"}
+product_fields["EN_REGISTRO"]["fields"] = {
 }
 
-graph_fields["EN_PROD_NORMA"] = {"alias": "rule", "sub_section": "details"}
-graph_fields["EN_PROD_NORMA"]["fields"] = {
+product_fields["EN_PROD_NORMA"] = {"alias": "rule", "sub_section": "details"}
+product_fields["EN_PROD_NORMA"]["fields"] = {
 }
 
-graph_fields["EN_PROD_VEGETAL"] = {
+product_fields["EN_PROD_VEGETAL"] = {
     "alias": "plant_product", "sub_section": "details"}
-graph_fields["EN_PROD_VEGETAL"]["fields"] = {
+product_fields["EN_PROD_VEGETAL"]["fields"] = {
 }
 
-graph_fields["EN_PROD_ARTISTICA"] = {
+product_fields["EN_PROD_ARTISTICA"] = {
     "alias": "prod_art", "sub_section": "details"}
-graph_fields["EN_PROD_ARTISTICA"]["fields"] = {
+product_fields["EN_PROD_ARTISTICA"]["fields"] = {
 }
 
-
-graph_fields["EN_PROD_ARTISTICA_DETALLE"] = {
+product_fields["EN_PROD_ARTISTICA_DETALLE"] = {
     "alias": "prod_art_detail", "sub_section": "details"}
-graph_fields["EN_PROD_ARTISTICA_DETALLE"]["fields"] = {
+product_fields["EN_PROD_ARTISTICA_DETALLE"]["fields"] = {
 }
 
-graph_fields["EN_PROD_OBRA_ARTISTICA"] = {
+product_fields["EN_PROD_OBRA_ARTISTICA"] = {
     "alias": "prod_artwork", "sub_section": "details"}
-graph_fields["EN_PROD_OBRA_ARTISTICA"]["fields"] = {
+product_fields["EN_PROD_OBRA_ARTISTICA"]["fields"] = {
 }
 
-
-graph_fields["EN_TESIS_ORIENTADA"] = {
+product_fields["EN_TESIS_ORIENTADA"] = {
     "alias": "oriented_thesis", "sub_section": "details"}
-graph_fields["EN_TESIS_ORIENTADA"]["fields"] = {
+product_fields["EN_TESIS_ORIENTADA"]["fields"] = {
 }
 
-graph_fields["EN_PROD_AUDIOVISUAL"] = {
+product_fields["EN_PROD_AUDIOVISUAL"] = {
     "alias": "audiovisual", "sub_section": "details"}
-graph_fields["EN_PROD_AUDIOVISUAL"]["fields"] = {
+product_fields["EN_PROD_AUDIOVISUAL"]["fields"] = {
 }
 
-
-graph_fields["EN_PROD_PARTITURA"] = {
+product_fields["EN_PROD_PARTITURA"] = {
     "alias": "music_sheet", "sub_section": "details"}
-graph_fields["EN_PROD_PARTITURA"]["fields"] = {
+product_fields["EN_PROD_PARTITURA"]["fields"] = {
 }
 
-graph_fields["EN_PROD_CAPITULO_LIBRO"] = {
+product_fields["EN_PROD_CAPITULO_LIBRO"] = {
     "alias": "book_chapter", "sub_section": "details"}
-graph_fields["EN_PROD_CAPITULO_LIBRO"]["fields"] = {
+product_fields["EN_PROD_CAPITULO_LIBRO"]["fields"] = {
 }
 
-graph_fields["EN_LIBRO"] = {"alias": "book", "sub_section": "details"}
-graph_fields["EN_LIBRO"]["fields"] = {
+product_fields["EN_LIBRO"] = {"alias": "book", "sub_section": "details"}
+product_fields["EN_LIBRO"]["fields"] = {
 }
 
-graph_fields["EN_PROD_ARTICULO"] = {
+product_fields["EN_PROD_ARTICULO"] = {
     "alias": "article", "sub_section": "details"}
-graph_fields["EN_PROD_ARTICULO"]["fields"] = {
+product_fields["EN_PROD_ARTICULO"]["fields"] = {
 }
 
-graph_fields["EN_REVISTA"] = {"alias": "journal", "sub_section": "details"}
-graph_fields["EN_REVISTA"]["fields"] = {
+product_fields["EN_REVISTA"] = {"alias": "journal", "sub_section": "details"}
+product_fields["EN_REVISTA"]["fields"] = {
 }
 
-graph_fields["EN_REVISTA_OTRA"] = {
+product_fields["EN_REVISTA_OTRA"] = {
     "alias": "journal_others", "sub_section": "details"}
-graph_fields["EN_REVISTA_OTRA"]["fields"] = {
+product_fields["EN_REVISTA_OTRA"]["fields"] = {
 }
 
-graph_fields["EN_EDITORIAL"] = {"alias": "editorial", "sub_section": "details"}
-graph_fields["EN_EDITORIAL"]["fields"] = {
+product_fields["EN_EDITORIAL"] = {
+    "alias": "editorial", "sub_section": "details"}
+product_fields["EN_EDITORIAL"]["fields"] = {
 }
 
-graph_fields["EN_EDITORIAL_OTRO"] = {
+product_fields["EN_EDITORIAL_OTRO"] = {
     "alias": "editorial_others", "sub_section": "details"}
-graph_fields["EN_EDITORIAL_OTRO"]["fields"] = {
+product_fields["EN_EDITORIAL_OTRO"]["fields"] = {
 }
 
-graph_fields["EN_SECTOR_APLICACION"] = {
+product_fields["EN_SECTOR_APLICACION"] = {
     "alias": "application_sector", "sub_section": "details"}
-graph_fields["EN_SECTOR_APLICACION"]["fields"] = {
+product_fields["EN_SECTOR_APLICACION"]["fields"] = {
 }
-graph_fields["EN_PALABRA_CLAVE"] = {
+product_fields["EN_PALABRA_CLAVE"] = {
     "alias": "keywords", "sub_section": "details"}
-graph_fields["EN_PALABRA_CLAVE"]["fields"] = {
+product_fields["EN_PALABRA_CLAVE"]["fields"] = {
 }
+
+
+##
+# networks fields
+##
+network_fields = {}
+network_fields["EN_RED"] = {"alias": "network"}
+network_fields["EN_RED"]["fields"] = {
+}
+
+network_fields["EN_INSTITUCION"] = {"alias": "institution"}
+network_fields["EN_INSTITUCION"]["fields"] = {
+}
+
+network_fields["RE_INSTITUCION_RED"] = {"alias": "institution_others"}
+network_fields["RE_INSTITUCION_RED"]["fields"] = {
+}
+
+
+network_fields["EN_RECURSO_HUMANO_OTRO"] = {"alias": "author_others"}
+network_fields["EN_RECURSO_HUMANO_OTRO"]["fields"] = {
+}
+
+network_fields["EN_COMUNIDAD"] = {
+    "alias": "community", "sub_section": "details"}
+network_fields["EN_COMUNIDAD"]["fields"] = {
+}
+network_fields["EN_PRODUCTO"] = {"alias": "product"}
+network_fields["EN_PRODUCTO"]["fields"] = {
+}
+
+network_fields["EN_MUNICIPIO"] = {"alias": "city"}
+network_fields["EN_MUNICIPIO"]["fields"] = {
+}
+network_fields["EN_PROYECTO"] = {"alias": "project"}
+network_fields["EN_PROYECTO"]["fields"] = {
+}
+
+network_fields["EN_GRUPO_PESQUISA"] = {"alias": "group"}
+network_fields["EN_GRUPO_PESQUISA"]["fields"] = {
+}
+
+network_fields["EN_AREA_CONOCIMIENTO"] = {"alias": "knowledge_area"}
+network_fields["EN_AREA_CONOCIMIENTO"]["fields"] = {
+}
+
+# adding fields for every main entity
+graph_fields["EN_PRODUCTO"] = product_fields
+graph_fields["EN_RED"] = network_fields
