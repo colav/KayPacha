2265f09cfdb1f7de711eb2900e5e9a930e9cffde
Omar Zapata <Omar.Zapata@cern.ch>
2022-08-09 06:39:40 -0500

    Working in migration for scienti 2022 and the new entity event

---
+++ kaypacha/models/scienti/2022/graph_schema.py
@@ -0,0 +1,15 @@
+# Scienti data model 2022
+# SCHEMA version for modification on the relations, it is vertioned as well
+from .graph_schema_product import graph_product
+from .graph_schema_network import graph_network
+from .graph_schema_project import graph_project
+from .graph_schema_event import graph_event
+
+graph_schema = {"SCIENTI_MODEL": 2022}
+graph_schema["MODELS"] = {}
+
+
+graph_schema["MODELS"]["network"] = graph_network
+graph_schema["MODELS"]["product"] = graph_product
+graph_schema["MODELS"]["project"] = graph_project
+graph_schema["MODELS"]["event"] = graph_event
