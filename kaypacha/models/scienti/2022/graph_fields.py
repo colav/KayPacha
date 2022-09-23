010b93a5e1e815f9e2f47e52d499f1e3f46f0001
Omar Zapata <Omar.Zapata@cern.ch>
2022-09-23 17:17:38 -0500

    added relations for patents in scienti.

--- kaypacha/models/scienti/2022/graph_fields.py
+++ kaypacha/models/scienti/2022/graph_fields.py
@@ -2,6 +2,7 @@ from .graph_fields_product import product_fields
 from .graph_fields_network import network_fields
 from .graph_fields_project import project_fields
 from .graph_fields_event import event_fields
+from .graph_fields_patent import patent_fields
 graph_fields = {}
 
 # adding fields for every main entity
@@ -9,3 +10,4 @@ graph_fields["EN_PRODUCTO"] = product_fields
 graph_fields["EN_RED"] = network_fields
 graph_fields["EN_PROYECTO"] = project_fields
 graph_fields["EN_EVENTO"] = event_fields
+graph_fields["EN_PATENTE"] = patent_fields
