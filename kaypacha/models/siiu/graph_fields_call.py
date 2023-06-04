8b24959b41c60b7014b266b2df823e780812db78
Omar Zapata <Omar.Zapata@cern.ch>
2023-06-03 19:42:33 -0500

    [SIIU] added tables SIIU_FECHA_PROCESO_SELECCION, SIIU_FECHA_INTERMEDIA, SIIU_ETAPA_PROCESO_SELECCION, SIIU_INSTANCIA_ADMINISTRATIVA

--- kaypacha/models/siiu/graph_fields_call.py
+++ kaypacha/models/siiu/graph_fields_call.py
@@ -13,3 +13,22 @@ call_fields["SIIU_PROYECTO"]["fields"] = {
 call_fields["SIIU_MODALIDAD_CONVOCATORIA"] = {"alias": "call_modality"}
 call_fields["SIIU_MODALIDAD_CONVOCATORIA"]["fields"] = {
 }
+
+call_fields["SIIU_FECHA_INTERMEDIA"] = {"alias": "middle_date"}
+call_fields["SIIU_FECHA_INTERMEDIA"]["fields"] = {
+}
+
+call_fields["SIIU_FECHA_PROCESO_SELECCION"] = {
+    "alias": "selection_process_date"}
+call_fields["SIIU_FECHA_PROCESO_SELECCION"]["fields"] = {
+}
+
+call_fields["SIIU_ETAPA_PROCESO_SELECCION"] = {
+    "alias": "selection_process_stage"}
+call_fields["SIIU_ETAPA_PROCESO_SELECCION"]["fields"] = {
+}
+
+call_fields["SIIU_INSTANCIA_ADMINISTRATIVA"] = {
+    "alias": "administrative_instance"}
+call_fields["SIIU_INSTANCIA_ADMINISTRATIVA"]["fields"] = {
+}
--- kaypacha/models/siiu/graph_schema_call.py
+++ kaypacha/models/siiu/graph_schema_call.py
@@ -9,7 +9,36 @@ graph_call = {
                     "KEYS": ["IDENTIFICADOR/CONVOCATORIA"],
                     "DB":"BUPP",
                     "TABLES":[
-                        {"SIIU_PROYECTO": None}
+                        {"SIIU_PROYECTO": [{
+                            "KEYS": ["MODALIDAD_CONVOCATORIA/IDENTIFICADOR"],
+                            "DB":"BUPP",
+                            "TABLES":[
+                                {"SIIU_MODALIDAD_CONVOCATORIA": None}
+                            ]
+                        }
+                        ]}
+                    ]
+                },
+                {
+                    "KEYS": ["IDENTIFICADOR/CONVOCATORIA"],
+                    "DB":"BUPP",
+                    "TABLES":[
+                        {"SIIU_FECHA_INTERMEDIA": [{
+                            "KEYS": ["IDENTIFICADOR"],
+                            "DB":"BUPP",
+                            "TABLES":[
+                                {"SIIU_FECHA_PROCESO_SELECCION": [{
+                                 "KEYS": ["ETAPA_PROCESO_SELECCION/IDENTIFICADOR"],
+                                 "DB":"BUPP",
+                                 "TABLES":[{"SIIU_ETAPA_PROCESO_SELECCION": [{
+                                     "KEYS": ["INSTANCIA_ADMINISTRATIVA/IDENTIFICADOR"],
+                                     "DB":"BUPP",
+                                     "TABLES":[{"SIIU_INSTANCIA_ADMINISTRATIVA": None}]
+                                 }]}]
+                                 }
+                                ]}]
+                        }]
+                        }
                     ]
                 },
 
